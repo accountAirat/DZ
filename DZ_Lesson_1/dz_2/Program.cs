bcc1b285a8bd4b1782765a622e07e23493af0a23
Airat <mingaleev.1993@mail.ru>
2022-09-17 19:06:10 +0300

    Выполнил второе задание.

---
+++ DZ_Lesson_1/dz_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел.*/
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+Console.Write("Введите второе число: ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+Console.Write("Введите третье число: ");
+string inputC = Console.ReadLine();
+int numberC = int.Parse(inputC);
+
+int max = numberA;
+
+if (max < numberB)
+    max = numberB;
+if (max < numberC)
+    max = numberC;
+
+Console.Write($"max={max}.");
