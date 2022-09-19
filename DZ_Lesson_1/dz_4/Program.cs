cf5fb2551425d2ed5953ec704c98804403f8faf0
Airat <mingaleev.1993@mail.ru>
2022-09-19 17:04:59 +0300

    Выполнил четвёртое задание

---
+++ DZ_Lesson_1/dz_4/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+int i = 1;
+
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+
+while (i < number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+
+}
