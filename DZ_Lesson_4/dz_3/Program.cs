0a307d241fb40cc20ae3ea7e17602b082d4103d1
Airat <mingaleev.1993@mail.ru>
2022-09-29 12:34:27 +0300

    В третьем задание, перевёл всё в строки

--- DZ_Lesson_4/dz_3/Program.cs
+++ DZ_Lesson_4/dz_3/Program.cs
@@ -5,16 +5,17 @@
 - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
 - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console.
 Только в основном блоке кода или в функции вывода*/
-void InputArray(int[] arr)
+/*Версия 2.0 принимает не только цифры, а элементы*/
+void InputArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"Введите {(i+1)} элемент из 8: ");
-        arr[i] = int.Parse(Console.ReadLine());
+        arr[i] = Console.ReadLine()!;
         //Нарушил правило "Не применять Console в функции", для добавления ввода массива 
     }
 }
-void PrintArray(int[] arr)
+void PrintArray(string[] arr)
 {
     Console.WriteLine("PrintArray: ");
     for (int i = 0; i < arr.Length; i++)
@@ -27,47 +28,49 @@ void PrintArray(int[] arr)
     }
     Console.WriteLine(" ");
 }
-void FillArray(int[] arr)
+void FillArray(string[] arr)
 {
     Random randGenerator = new Random();
+    int cache = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = randGenerator.Next(0,10);
+        cache = randGenerator.Next(0,10);
+        arr[i] = $"{cache}";
     }
 }
-
 Console.Clear();
 int i = 1;
-int[] array = new int[8];
-int menu = 0;
+string[] array = new string[8];
+string menu = String.Empty;
 while (i > 0)
 {
-    if (menu == 0)
+    if (menu == "0")
+    Console.WriteLine(" ");//Почему съедается эта строка в первом выводе?
     Console.WriteLine("1. Ввести массив");
     Console.WriteLine("2. Случайный массив");
-    Console.WriteLine("3. Выйти");
+    Console.WriteLine("Q. Выйти");
     Console.Write("Введите нужную цифру: ");
-    menu = int.Parse(Console.ReadLine());
-    if (menu == 1)
+    menu = Console.ReadLine()!;
+    if (menu == "1")
     {
           InputArray(array);
           PrintArray(array);
-          menu = 0;
+          menu = "0";
     }
-    else if (menu == 2)
+    else if (menu == "2")
     {
           FillArray(array);
           PrintArray(array);
-          menu = 0;
+          menu = "0";
     }
-    else if (menu == 3)
+    else if (menu == "q" || menu == "Q")
     {
         break;
     }
     else
     {
         Console.WriteLine("Ввели не верный символ, попробуйте ещё раз.");
-        menu = 0;
+        menu = "0";
     }
 }
 Console.WriteLine("До встречи!");
