73505eb52aa17c2a7a2a05ffdfe28532cca35953
Airat <mingaleev.1993@mail.ru>
2022-09-30 12:29:53 +0300

    First exercise done

--- DZ_Lesson_5/1/Program.cs
+++ DZ_Lesson_5/1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+void ArrayFill(int[] arr)
+{
+    Random generator = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = generator.Next(100, 1000);
+    }
+}
+int CheckArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+int[] array = new int[10];
+ArrayFill(array);
+Console.Write($"{String.Join(", ", array)} -> {CheckArray(array)}");
