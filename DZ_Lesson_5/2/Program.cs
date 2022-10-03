08a85851ea44b3fef1207f5da2da256a03177dac
Airat <mingaleev.1993@mail.ru>
2022-10-03 09:27:18 +0300

    выполнил второе и  третье задание

---
+++ DZ_Lesson_5/2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+void ArrayFill(int[] arr)
+{
+    Random generator = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = generator.Next(-100, 100);
+    }
+}
+int CheckArray(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum += arr[i];
+    }
+    return sum;
+}
+int[] array = new int[10];
+ArrayFill(array);
+Console.Write($"{String.Join(", ", array)} -> {CheckArray(array)}");
---
+++ DZ_Lesson_5/3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+void FillDoubleArray(double[] arr)
+{
+    Random generator = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = generator.NextDouble();
+    }
+}
+double CheckArrayMaxMin(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+     if (max < arr[i])
+     {
+        max = arr[i];
+     }   
+     else if (min > arr [i])
+     {
+        min = arr [i];
+     }
+    }
+
+    double result = max - min;
+    return result;
+}
+double[] array = new double[10];
+FillDoubleArray(array);
+Console.Write($"{String.Join(", ", array)} -> {CheckArrayMaxMin(array)}");
