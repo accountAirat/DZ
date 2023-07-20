2f498c8c0f55a118e812c2f26ab233cee07af4bc
Airat <mingaleev.1993@mail.ru>
2023-07-20 12:32:32 +0300

    Save commit

---
+++ DZ_Lesson_9/dz_4/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+В массиве info хранятся двоичные представления нескольких чисел (без разделителя).
+В массиве data хранится информация о количестве бит, которые занимают числа из массива info.
+Напишите программу, которая составит массив десятичных представлений чисел массива data
+с учётом информации из массива info.
+входные данные: data = {(0, 1,) (1, 1, 1,) (0, 0, 0), (1) } info = {2, (3), 3, 1 }выходные данные: 1, (7), 0, 1
+*//*
+int[] Convertеr(int[] binary, int[] bitCount)
+{
+    int number = 0;
+    int j = 0;
+    for (int i = 0; i < binary.Length; i++)
+    {
+        if (bitCount[j] == 0)
+        {
+            bitCount[j] = number;
+            number = 0;
+            j++;
+        }
+        else
+        {
+            number = number + (int)Math.Pow(2, --(bitCount[j])) * binary[i];
+        }
+    }
+    return bitCount;
+}*/
+int[] Convertеr(int[] binary, int[] bitCount)
+{
+    int number = 0;
+    for (int j = 0; j < binary.Length; j++)
+    {
+        for (int i = 0; i < bitCounter[]; i++)
+        {
+            if (bitCount[j] == 0)
+            {
+                bitCount[j] = number;
+                number = 0;
+                j++;
+            }
+            else
+            {
+                number = number + (int)Math.Pow(2, --(bitCount[j])) * binary[i];
+            }
+        }
+    return bitCount;
+}
+Console.Clear();
+int[] binary = { 0, 1, 1, 1, 1, 0, 0, 0, 1};
+int[] bitCount = { 2, 3, 3, 1};
+Console.WriteLine(string.Join(", ", Convertеr(binary, bitCount)));
+
+
---
+++ temp/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] FillNewArray(string[] arr, string[] newArr, int standard)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= standard)
+        {
+            newArr[count] = arr[i];
+            count++;
+        }
+    }
+    return newArr;
+}
+FillNewArray(array, newArray, standardSize);
+Console.WriteLine(string.Join(", ", newArray));
+string[] newArray = new string[ElementCounting(array, standardSize)];
